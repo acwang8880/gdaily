fa29d65b63ff9ee5564dbc296c75bc04dae03c43
Alex Wang <alexwang8880@gmail.com>
2021-07-03 00:43:26 -0400

    Add date to cron log

--- cron-initializer.sh
+++ cron-initializer.sh
@@ -4,7 +4,8 @@ PYTHON=/home/cptbirdy/.virtualenvs/genshin/bin/python3
 SCRIPT=/home/cptbirdy/proj/personal/genshin/genshin_login.py
 LOG=/tmp/.trash/genshin.log
 
-$PYTHON $SCRIPT 2>&1 > $LOG
+date > $LOG
+$PYTHON $SCRIPT 2>&1 >> $LOG
 
 # source /home/cptbirdy/.virtualenvs/genshin/bin/activate
 # python /home/cptbirdy/proj/personal/genshin/genshin/genshin_login.py 2>&1 > $LOG
